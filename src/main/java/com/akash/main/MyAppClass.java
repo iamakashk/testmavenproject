477680779d44da4a2a875435722be9db5e7c5bad
iamakashk <kambleakashg@gmail.com>
2025-02-08 14:57:26 +0530

    first commit

---
+++ src/main/java/com/akash/main/MyAppClass.java
@@ -0,0 +1,11 @@
+package com.akash.main;
+
+public class MyAppClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print(" Hello World....");
+	}
+
+}
