a50627c29dcb7dc9f7a0e1229446172d75cdbfb6
iamakashk <kambleakashg@gmail.com>
2025-02-09 10:01:29 +0530

    I have added new feature

--- src/main/java/com/akash/main/MyAppClass.java
+++ src/main/java/com/akash/main/MyAppClass.java
@@ -5,7 +5,7 @@ public class MyAppClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print(" This is my feature 2 ");
+		System.out.print(" Hello Good Morning, This is sunday !!!!! This is my feature 2  ");
 	}
 
 }
