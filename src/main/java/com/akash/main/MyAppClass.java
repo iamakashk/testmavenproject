4126030f0737975fa40ecf893b585b4f3cd18d25
iamakashk <kambleakashg@gmail.com>
2025-02-11 20:06:53 +0530

    akash has added something cool

--- src/main/java/com/akash/main/MyAppClass.java
+++ src/main/java/com/akash/main/MyAppClass.java
@@ -5,12 +5,12 @@ public class MyAppClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.print(" Hello Good Morning, This is sunday !!!!! This is my feature 2  ");
+
 		
-		System.out.println("      ");
+		
 		
 		for(int i = 1 ; i < 11 ; i++) {
-			System.out.println(i + " I am learning Jenkins with harsh and suyash..........");
+			System.out.println(i + " Hi this is my new feature...hahahahahahaha ..........");
 		}
 	}
 
